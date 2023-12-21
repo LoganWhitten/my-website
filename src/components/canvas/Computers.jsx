f32ce8da7ea1e48a4a1debcfbfc5f132a90dcad1
lights9999 <32558210+lights9999@users.noreply.github.com>
2023-12-20 22:59:07 -0600

    added slash

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("src/assets/Band Stage.gltf");
+  const computer = useGLTF("/src/assets/Band Stage.gltf");
 
   return (
     <mesh>
