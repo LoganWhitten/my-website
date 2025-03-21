7097a1fa1e9e6d72754cd820b449b522aae76297
lights9999 <32558210+LoganWhitten@users.noreply.github.com>
2025-03-20 21:56:13 -0400

    a start...

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/hero/hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-800 h-full">
+      <Hero />
+    </div>
+  );
+}
